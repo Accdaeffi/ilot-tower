27e1ae7dc8df50208642d01bd34d012688d69d20
Дмитрий Носков <noskoff2@yandex.ru>
2024-01-02 18:34:06 +0300

    init

---
+++ src/main/java/ru/ilot/ilottower/IlottowerApplication.java
@@ -0,0 +1,13 @@
+package ru.ilot.ilottower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlottowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlottowerApplication.class, args);
+	}
+
+}
